628a8e7ae8d37bea1587f21885345abe491a3447
kannyekong <31696884+kannyekong@users.noreply.github.com>
2023-03-13 15:06:41 +0100

    FIX: fixed mobile  responsiveness of users page

--- src/assets/Components/DashboardContents.jsx
+++ src/assets/Components/DashboardContents.jsx
@@ -8,9 +8,9 @@ import Table from "./Table";
 
 const DashboardContent = () => {
   return (
-    <div className="p-16 w-full">
+    <div className="p-6 md:p-16 w-full">
       <div className="space-y-8 min-w-full">
-        <p className="text-[24px] ml-2 font-bold">Users</p>
+        <p className="md:text-[24px] ml-2 font-bold">Users</p>
         <div className="flex flex-col items-center justify-center">
           <div className="flex flex-col md:flex-row w-full space-y-4 md:space-y-0 md:space-x-12">
             <CardItem src={one} description="USERS" figures="100" />
